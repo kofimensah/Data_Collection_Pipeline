3c83bade14945af19626939d4359330f0f3bbf75
kofimensah <kofi.mensah@hotmail.com>
2022-06-07 23:51:25 +0100

    Included methods to navigate website

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 from time import sleep
 
@@ -10,4 +12,30 @@ class Scraper:
     def connect_2_webpage(self, url):
         driver = webdriver.Chrome()
         driver.get(url)
-        
+    
+    def next_page(self, page_number):
+        # locate button id=page_number
+        # click button
+        'to be defined'
+        pass
+
+    def clear_cookies(self, id):
+        # search for cookies element on page
+        # get cookies element
+        # search for button with id
+        # click to accept all cookies
+        pass
+
+    def get_container_data(self):
+        pass
+
+    def search(self, driver, search_item='London'):
+        '''
+        Function for searching in a search field on a webpage.
+        '''
+        elem = driver.find_element(By.NAME,"typeAheadInputField")
+        elem.clear()    # Clear the search field
+        elem.send_keys(search_item)     #type search_item='London' in the search field
+        elem.send_keys(Keys.RETURN)     #Click enter in the search field
+
+  
