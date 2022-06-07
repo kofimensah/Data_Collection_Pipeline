519b02ccb1f4fe029c8873390568c5cfb4bbcff9
kofimensah <kofi.mensah@hotmail.com>
2022-06-07 23:54:50 +0100

    updates" updates
    f

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Data_Collection_Pipeline
 An implementation of an industry grade data collection pipeline that runs scalably in the cloud.
+
+to be updated  
--- scraper.py
+++ scraper.py
@@ -27,7 +27,11 @@ class Scraper:
         pass
 
     def get_container_data(self):
-        pass
+        driver = webdriver.Chrome()
+        driver.get("https://www.zoopla.co.uk/for-sale/property/london/?q=London&results_sort=newest_listings&search_source=home")
+        houses_container = driver.find_elements(by=By.XPATH, value="//div[@class='css-1itfubx e1c4dna40']//div[@class='e1c4dna41 css-dhlvpc e1h9td4l19']")
+        for house in houses_container:
+            print(house.find_element(by=By.XPATH, value=".//p[@class='css-1w7anck e1h9td4l31']").text)
 
     def search(self, driver, search_item='London'):
         '''
@@ -38,4 +42,11 @@ class Scraper:
         elem.send_keys(search_item)     #type search_item='London' in the search field
         elem.send_keys(Keys.RETURN)     #Click enter in the search field
 
-  
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+driver = webdriver.Chrome()
+driver.get("https://www.zoopla.co.uk/for-sale/property/london/?q=London&results_sort=newest_listings&search_source=home")
+houses_container = driver.find_elements(by=By.XPATH, value="//div[@class='css-1itfubx e1c4dna40']//div[@class='e1c4dna41 css-dhlvpc e1h9td4l19']")
+for house in houses_container:
+    print(house.find_element(by=By.XPATH, value=".//p[@class='css-1w7anck e1h9td4l31']").text)
