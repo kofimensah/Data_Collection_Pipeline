c1209b9bd5237983e203a8710694d03114f7e054
kofimensah <kofi.mensah@hotmail.com>
2022-06-19 16:28:44 +0100

    Creating function to deal with alerts and navigate to desired pages

--- script_full_temp.py
+++ script_full_temp.py
@@ -1,31 +1,37 @@
-'''
-this will serve as my test center where i will put the full code without any classes.
-
-Eventually, I will use this space to test the class
-
-Target: F1 team -> standings -> merch
-'''
-
-
-'''
-Ideas: Use selenium to navigate to my twitter page and data mine word useage and types of content people tweet
-trends in Tweets
-'''
-
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium import webdriver
 from time import sleep
 
 # method to access website
-driver = webdriver.Chrome()
-url = 'https://www.formula1.com/'
-driver.get(url)
+def website_pointer(url):
+    driver = webdriver.Chrome()
+    driver.get(url)
+    sleep(2)
+    return driver
+
+#method to by-pass cookies
+def accept_all_cookies(driver, text):
+    try:
+        cookies_button = driver.find_element(By.XPATH, f'//button[text()="{str(text)}"]')
+        cookies_button.click()
+    except:
+        pass
+
+driver = website_pointer('https://www.formula1.com/')
+accept_all_cookies(driver, "Accept All")
+
+
 
 # method to navigate to desired pages - by link or by click
-teams_page = driver.find_elements(by=By.XPATH, value="//div[@class='css-1itfubx e1c4dna40']//div[@class='e1c4dna41 css-dhlvpc e1h9td4l19']")
-for team in teams_page:
-    print(team.find_element(by=By.XPATH, value=".//p[@class='css-1w7anck e1h9td4l31']").text)
+driver.find_elements(By.XPATH, f"//span[text()='Teams']").click()
 
-# method to deal with pop-ups
+#for team in teams_page:
+#    print(team.find_element(by=By.XPATH, value=".//d[@class='css-1w7anck e1h9td4l31']").text)
 
+
+#method to deal with pop-ups
+alert = driver.switch_to.alert
+
+#close driver
+driver.close()
