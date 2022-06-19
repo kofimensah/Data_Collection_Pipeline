73cf5b5eb78c691b9036c4447dbcea3932378709
kofimensah <kofi.mensah@hotmail.com>
2022-06-19 16:27:21 +0100

    Updated mothods to by-pass cookies and access main webpage"

--- scraper.py
+++ scraper.py
@@ -8,23 +8,26 @@ class Scraper:
     def __init__(self, url):
         self.url = url
 
-    
     def connect_2_webpage(self, url):
         driver = webdriver.Chrome()
         driver.get(url)
-    
+        sleep(2)
+        return driver
+
     def next_page(self, page_number):
+        next_page = driver.find_element(By.XPATH, f'//button')
         # locate button id=page_number
         # click button
         'to be defined'
         pass
 
-    def clear_cookies(self, id):
-        # search for cookies element on page
-        # get cookies element
-        # search for button with id
-        # click to accept all cookies
-        pass
+    #method to accept all cookies by accepting the text on the button as an argument
+    def accept_all_cookies(self, button_text):
+        try:
+            cookies_button = driver.find_element(By.XPATH, f'//button[text()="{str(button_text)}"]')
+            cookies_button.click()
+        except:
+            pass
 
     def get_container_data(self):
         driver = webdriver.Chrome()
@@ -51,3 +54,4 @@ driver.get("https://www.zoopla.co.uk/for-sale/property/london/?q=London&results_
 houses_container = driver.find_elements(by=By.XPATH, value="//div[@class='css-1itfubx e1c4dna40']//div[@class='e1c4dna41 css-dhlvpc e1h9td4l19']")
 for house in houses_container:
     print(house.find_element(by=By.XPATH, value=".//p[@class='css-1w7anck e1h9td4l31']").text)
+
